bab3d25340dcef62370cceedd3d4bc1bb7d5e9d1
Frank Fiorante <frankfiorante@Franks-MacBook-Air.local>
2020-11-12 21:32:24 -0600

    initial commit

---
+++ src/main/java/com/frankfiorante/RTX3070Bot/Rtx3070BotApplication.java
@@ -0,0 +1,13 @@
+package com.frankfiorante.RTX3070Bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Rtx3070BotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Rtx3070BotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/frankfiorante/RTX3070Bot/Rtx3070BotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.frankfiorante.RTX3070Bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Rtx3070BotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
